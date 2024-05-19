b5e01e19e0a42af9d6a438d79e12782f5208ec2f
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-05-19 18:34:40 +0300

    Add files via upload

---
+++ MOD-1_homework2.py
@@ -0,0 +1,18 @@
+
+# Практическая работа по уроку "Динамическая типизация"
+
+
+
+
+
+name = 'Yuriy'
+print('Name: ' + name)
+age = int(44)
+print('Age: ' + str(age))
+age = (age + 1)
+print('New age: ' + str(age))
+is_student = True
+print('Is Student: ' + str(is_student))
+
+
+
